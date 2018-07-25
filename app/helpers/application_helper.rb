0230f0e962e8f4478fed7c71488951b838478cac
Warsama Gabriel <srobleh@gmail.com>
2018-07-24 19:58:57 -0700

    Updated master layour styles along with helper methods.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
 	#html_safe => rails "scrubs" away the html just to return the inner string for protection
 
-	def login_helper
+	def login_helper style
 		#remove condition logic from the views! -->
     #user class can either be of class User or Openstruct -->
     if current_user.is_a?(User)
-      link_to 'Logout', destroy_user_session_path, method: :delete
+      link_to 'Logout', destroy_user_session_path, method: :delete, class: style
     else
-      (link_to 'Login', new_user_session_path) + ' | ' +
-      (link_to 'SignUp', new_user_registration_path) 
+      (link_to 'Register', new_user_registration_path, class: style) + " " +
+      (link_to 'Login', new_user_session_path, class: style) 
     end
 	end
 
@@ -20,6 +20,6 @@ module ApplicationHelper
 
 
   def set_copyright
-    @copyright = WGViewTool::Renderer.copyright('Warsama Gabriel', 'All rights reserved')
+    WGViewTool::Renderer.copyright('Warsama Gabriel', 'All rights reserved')
   end
 end
