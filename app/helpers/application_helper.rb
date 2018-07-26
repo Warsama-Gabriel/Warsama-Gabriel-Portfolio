24acd321f90af6b5a572f561c4bbec75032c9355
Warsama Gabriel <srobleh@gmail.com>
2018-07-25 18:41:14 -0700

    More minor modifications

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,15 @@
 module ApplicationHelper
 	#html_safe => rails "scrubs" away the html just to return the inner string for protection
 
-	def login_helper style=''
-		#remove condition logic from the views! -->
-    #user class can either be of class User or Openstruct -->
-
-    if current_user.is_a?(User)
-      link_to 'Logout', destroy_user_session_path, method: :delete, class: style
+	def login_helper style = ''
+    if current_user.is_a?(GuestUser)
+      (link_to "Register", new_user_registration_path, class: style) +
+      " ".html_safe +
+      (link_to "Login", new_user_session_path, class: style)
     else
-      (link_to 'Register', new_user_registration_path, class: style) + " " +
-      (link_to 'Login', new_user_session_path, class: style) 
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
     end
-	end
+  end
 
 	def source_helper
 	 if session[:source]
