9279d8225b9b6907d3ad977946b636762e1d4bc2
Warsama Gabriel <srobleh@gmail.com>
2018-07-26 21:39:42 -0700

    styled navbar navlinks, added active class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,4 +21,44 @@ module ApplicationHelper
   def set_copyright
     WGViewTool::Renderer.copyright('Warsama Gabriel', 'All rights reserved')
   end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_me_path,
+        title: 'About'
+      },
+      {
+        url: blogs_path,
+        title: 'Blog'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio'
+      },
+    ]
+  end
+
+
+
+  def nav_helper style, tag_type
+    nav_links = ''
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+    puts nav_links
+    nav_links.html_safe #interpret the html before rendering it on the screen.
+  end
+
+  def active? path
+    "active" if current_page?(path)
+  end
 end
