821398587441a5eb90c64a8f31cc5983395c9604
Warsama Gabriel <srobleh@gmail.com>
2018-08-01 16:42:38 -0700

    Integrated cocoon gem into portfolio form.

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,11 +1,12 @@
 class Portfolio < ApplicationRecord
 
-	has_many :technologies
+	has_many :technologies, inverse_of: :portfolio
 	#accepts_nested_attributes_for allows us to save parent and child model all in one go.
 	#creates fields_for builder in our forms
 	#passing in a set of key value pairs in the technologies attributes as an array of hashes.
 	accepts_nested_attributes_for :technologies, 
-								   reject_if: lambda {|attrs| attrs['name'].blank? }
+																allow_destroy: true,
+								   							reject_if: lambda {|attrs| attrs['name'].blank? }
 
 	mount_uploader :thumb_image, PortfolioUploader
 	mount_uploader :main_image, PortfolioUploader
